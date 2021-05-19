417cf7b8520f635a01ea56251c889e7bf801e60c
Benjamin SELLEM <besellem@student.42.fr>
2021-05-19 14:27:05 +0200

    init structure

---
+++ incs/minishell.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 14:07:35 by besellem          #+#    #+#             */
+/*   Updated: 2021/05/19 14:24:52 by besellem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/*
+** -- INCLUDES --
+*/
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <dirent.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/errno.h>
+# include <sys/ioctl.h>
+
+# include "libft.h"
+
+/*
+** -- DEFINES --
+*/
+# define PROG_NAME "minishell"
+
+/*
+** -- DATA STRUCTURES --
+*/
+
+/*
+** -- PROTOTYPES --
+*/
+
+#endif
