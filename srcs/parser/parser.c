a657623cef11bde9949cc4e546a464221d068871
Benjamin Sellem <69735857+besellem@users.noreply.github.com>
2021-05-26 23:41:00 +0200

    new parser in progress

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 22:02:00 by besellem          #+#    #+#             */
-/*   Updated: 2021/05/26 16:15:26 by besellem         ###   ########.fr       */
+/*   Updated: 2021/05/26 23:40:11 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,6 @@
 
 #define COMPLETE_METACHARACTERS "|&;()<> \t"
 #define METACHARACTERS "|;<> \t"
-
-// void	ft_parse(char *s)
-// {
-// 	size_t	i;
-// 	size_t	j;
-
-// 	// ft_lstiter(singleton()->lst, free);		// free all `void *content' nodes
-// 	ft_lstclear(&singleton()->lst, free);	// free the list containing all parsed commands
-// 	quotes2close(0, RESET_FLAG);			// reinit the quote test
-// 	i = 0;
-// 	while (s[i])
-// 	{
-// 		j = i;
-// 		while (s[j])
-// 		{
-// 			ft_dprintf(STDERR_FILENO, "[%c]", s[j]);
-// 			// if (s[j] == '\\' && ft_incharset(" $'\"", s[j + 1]))
-// 			// {
-// 			// 	// s[j] = ' '; // replace `\' (backslash) by a space
-// 			// 	j += 2;
-// 			// }
-// 			quotes2close(s[j], SET_FLAG);	// add quote test on this char
-// 			if ((!quotes2close(0, SET_FLAG) && found_str_limit(s, i, j)))
-// 				break ;
-// 			++j;
-// 		}
-// 		ft_putstr_fd("\n", STDERR_FILENO);
-// 		i = j + 1;
-// 	}
-// 	// ft_lstprint_cmd(singleton()->lst);
-// }
-
 
 // RESET_FLAG is used to reset static variables in `quotes2close'
 #define RESET_FLAG 1
@@ -104,9 +72,6 @@ t_cmd	*new_cmd(uint16_t status, t_list **args)
 	}
 	cmd->args = ft_lst2strs(args);
 	ft_lstclear(args, free);
-	// *args = NULL;
-
-	// cmd->args = ft_split(s, ' '); 		// ############ TEMPORARY ##############
 	cmd->args_len = ft_strslen(cmd->args);
 	cmd->status_flag = status;
 	return (cmd);
@@ -114,7 +79,6 @@ t_cmd	*new_cmd(uint16_t status, t_list **args)
 
 int	found_str_limit(char *s, size_t i, t_list **args)
 {
-	// static - no use to redefine it at each call
 	static struct s_redirections	limits[] = {
 		{";", 1, FLG_EO_CMD}, {"|", 1, FLG_PIPE}, {">>", 2, FLG_APPEND},
 		{">", 1, FLG_OUTPUT}, {"<", 1, FLG_INPUT}, {NULL, 0, 0}
@@ -177,10 +141,7 @@ size_t	get_env_var(char **s, size_t i)
 	// 	return (ft_nblen(singleton()->last_return_value) - 1);
 	// }
 	if (NOT_FOUND == len_str)
-	{
-		// ft_printf(B_RED "VAR WRONG FORMAT\n" CLR_COLOR, __LINE__, len_str, i);
 		return (0);
-	}
 	p = ft_substr(*s, (unsigned int)i, len_str);
 	if (!p)
 		return ((size_t)ft_malloc_error(__FILE__, __LINE__));
@@ -201,6 +162,130 @@ size_t	get_env_var(char **s, size_t i)
 	else
 		return (-1);
 }
+
+
+/*******************************************************************************
+-------- NEW PARSER IN PROCESS --------
+*******************************************************************************/
+
+// #define SPEC_CHARS "\\$"
+// #define SPACES " \t"
+// #define ISSPACE_CHAR " tfvr"
+// #define QUOTES "\"'"
+
+// void	ft_parse(char *s)
+// {
+// 	t_quotes	quotes;
+// 	t_list		*args;
+// 	size_t		i;
+// 	int			limit;
+
+// 	args = NULL;
+// 	ft_lstclear(&singleton()->lst, free);
+// 	quotes2close(0, &quotes, RESET_FLAG);
+// 	i = 0;
+// 	while (s[i])
+// 	{
+// 		while (s[i] && ft_incharset(SPACES, s[i]) && quotes.first == 0)
+// 			++s;
+// 		if (!s[i])
+// 			break ;
+
+// 		quotes2close(s[i], &quotes, SET_FLAG);
+// 		if (quotes.first == 0)
+// 		{
+// 			limit = found_str_limit(s, i, &args);
+// 			if (limit)
+// 			{
+// 				quotes2close(0, &quotes, RESET_FLAG);
+// 				s += i + limit;
+// 				i = 0;
+// 				continue ;
+// 			}
+// 			if (s[i] == '\\')
+// 			{
+// 				ft_strnclean(s + i, "\\", 1);
+// 				if (ft_incharset(SPEC_CHARS, s[i]))//|| (ft_incharset(QUOTES, s[i]) && ))
+// 					++i;
+// 				continue ;
+// 			}
+// 		}
+		
+// 		// ft_printf("%s:%d: [%.4b] [%s]\n", __FILE__, __LINE__, quotes.first, s + i);
+
+// 		// quotes2close(s[i], &quotes, SET_FLAG);
+// 		if (s[i] == '\\' && quotes.first != 0)
+// 		{
+// 			PRINT_ERR("HERE")
+// 			if (quotes.first & (1 << DBL_BSHFT) && ('"' == s[i]))
+// 			{
+// 				ft_strnclean(s + i, "\\", 1);
+// 			}
+// 			else
+// 				++i;
+// 			if (ft_incharset(SPEC_CHARS, s[i]) || ft_incharset(QUOTES, s[i]))
+// 				++i;
+// 			continue ;
+// 		}
+// 		else if (ft_incharset(QUOTES, s[i]) && quotes.did_change)
+// 		{
+// 			ft_strnclean(s + i, QUOTES, 1);	// remove ``'"`` from `s'
+// 			continue ;
+// 			// if (quotes.first)
+// 			// {
+// 			// 	ft_lstadd_back(&args, ft_lstnew(ft_substr(s, 0, i)));
+// 			// 	s += i;
+// 			// 	i = 0;
+// 			// 	continue ;
+// 			// }
+// 			// if ((('"' == s[i]) && (('"' == s[i]) << DBL_BSHFT) & quotes.first)
+// 			// 	|| (('\'' == s[i]) && (('\'' == s[i]) << SGL_BSHFT) & quotes.first))
+// 			// 	ft_lstadd_back(&args, ft_lstnew(ft_strdup("")));
+// 			// continue ;
+// 		}
+// 		else if (s[i] == '$' && (!quotes.first || (quotes.first & (1 << DBL_BSHFT))))
+// 		{
+// 			++i;
+// 			i += get_env_var(&s, i);
+// 		}
+// 		else
+// 			++i;
+
+// 		// quotes2close(s[i], &quotes, SET_FLAG);
+// 		if ((ft_incharset(SPACES, s[i]) && quotes.first == 0))
+// 		{
+// 			ft_lstadd_back(&args, ft_lstnew(ft_substr(s, 0, i)));
+// 			s += i;
+// 			i = 0;
+// 		}
+// 		if ((('"' == s[i]) && (('"' == s[i]) << DBL_BSHFT) & quotes.first)
+// 		|| (('\'' == s[i]) && (('\'' == s[i]) << SGL_BSHFT) & quotes.first))
+// 		{
+// 			// ft_printf("s[i]: [%c], s: [%s], i: [%d]\n", s[i], s, i);
+// 			ft_lstadd_back(&args, ft_lstnew(ft_strdup("")));
+// 		}
+// 	}
+// 	if (!s[i])
+// 	{
+// 		t_cmd	*new;
+
+// 		if (i > 0 && s[i - 1])// && !ft_incharset(SPACES, s[i - 1]))
+// 			ft_lstadd_back(&args, ft_lstnew(ft_substr(s, 0, i)));
+// 		new = new_cmd(FLG_EOL, &args);
+// 		ft_lstadd_back(&singleton()->lst, ft_lstnew(new));
+// 	}
+// 	ft_lstprint_cmd(singleton()->lst);
+// 	ft_printf("\n");
+// }
+
+/*******************************************************************************
+-------- END NEW PARSER IN PROCESS --------
+*******************************************************************************/
+
+
+/*******************************************************************************
+-------- OLD PARSER DOING QUITE WELL --------
+*******************************************************************************/
 
 #define SPEC_CHARS " \\$'\""
 #define SPACES " \t"
@@ -267,9 +352,6 @@ void	ft_parse(char *s)
 			}
 			else if (s[i] == '$' && (!quotes.first || (quotes.first & (1 << DBL_BSHFT))))
 			{
-				/*
-				** search in env variables to replace `$' by its value
-				*/
 				++i;
 				i += get_env_var(&s, i);
 			}
@@ -299,14 +381,18 @@ void	ft_parse(char *s)
 	// ft_printf("\n");
 }
 
+/*******************************************************************************
+-------- END OLD PARSER DOING QUITE WELL --------
+*******************************************************************************/
+
+
+
 /*
 TESTS TO DO:
 
 echo \""''\"""bonjour"
 "''"bonjour
 "''"""bonjour
-
-echo \""''\"""bonjour"
 
 ls -la; echo bonjour >> out; ls | grep srcs ;
 ls -la; ec"ho" bo"njo"ur >> out ;echo ' bonjour "" "a\" $LESS\toi'\"\' > out
